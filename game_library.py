dd02732305a4f7d948b787c3170fc9859589785f
alvesgf16 <alvesgabriel1994@gmail.com>
2023-03-07 13:02:59 -0300

    refactor: encapsulate games in list

--- game_library.py
+++ game_library.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
 
 
+class Game:
+    def __init__(self, a_name: str) -> None:
+        self.__name = a_name
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+
 app = Flask(__name__)
 
 
 @app.route("/start")
 def hello() -> str:
-    games = ["Tetris", "Skyrim", "Crash Bandicoot"]
+    tetris = Game("Tetris")
+    skyrim = Game("Skyrim")
+    crash = Game("Crash Bandicoot")
+    games = [tetris, skyrim, crash]
     return render_template("list.html", a_title="Games", table_data=games)
 
 
