734785bda2a98e01081c72a5172a9c0377d3d224
alvesgf16 <alvesgabriel1994@gmail.com>
2023-03-08 12:30:36 -0300

    test: extract auxiliary functions

--- tests/test_game_library.py
+++ tests/test_game_library.py
@@ -8,17 +8,37 @@ class TestGameLibrary(unittest.TestCase):
         self.app = app.test_client()
 
     def test_page_header(self):
-        response = self.app.get("/start")
-        assert b"<h1>Games</h1>" in response.data
+        response = self.__when_the_test_client_calls_the_start_route()
+        self.__then_the_page_header_contains_the_correct_text(
+            response, b"<h1>Games</h1>"
+        )
 
     def test_table_data(self):
-        response = self.app.get("/start")
-        assert b"<td>Tetris</td>" in response.data
-        assert b"<td>Puzzle</td>" in response.data
-        assert b"<td>Atari</td>" in response.data
-        assert b"<td>Skyrim</td>" in response.data
-        assert b"<td>RPG</td>" in response.data
-        assert b"<td>PS3</td>" in response.data
-        assert b"<td>Crash Bandicoot</td>" in response.data
-        assert b"<td>Platform</td>" in response.data
-        assert b"<td>PS1</td>" in response.data
+        response = self.__when_the_test_client_calls_the_start_route()
+        self.__then_the_cells_in_a_table_line_contain_the_correct_data(
+            response, b"<td>Tetris</td>", b"<td>Puzzle</td>", b"<td>Atari</td>"
+        )
+        self.__then_the_cells_in_a_table_line_contain_the_correct_data(
+            response, b"<td>Skyrim</td>", b"<td>PS3</td>", b"<td>RPG</td>"
+        )
+        self.__then_the_cells_in_a_table_line_contain_the_correct_data(
+            response,
+            b"<td>Crash Bandicoot</td>",
+            b"<td>Platform</td>",
+            b"<td>PS1</td>",
+        )
+
+    def __when_the_test_client_calls_the_start_route(self):
+        return self.app.get("/start")
+
+    def __then_the_page_header_contains_the_correct_text(
+        self, response, header_text
+    ):
+        assert header_text in response.data
+
+    def __then_the_cells_in_a_table_line_contain_the_correct_data(
+        self, response, name_cell, genre_cell, platform_cell
+    ):
+        assert name_cell in response.data
+        assert genre_cell in response.data
+        assert platform_cell in response.data
