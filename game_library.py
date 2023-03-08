7b640441996f444649f9f345443713b025f94810
alvesgf16 <alvesgabriel1994@gmail.com>
2023-03-08 12:18:19 -0300

    feat: add more properties to game class

--- game_library.py
+++ game_library.py
@@ -2,12 +2,22 @@ from flask import Flask, render_template
 
 
 class Game:
-    def __init__(self, a_name: str) -> None:
+    def __init__(self, a_name: str, a_genre: str, a_platform: str) -> None:
         self.__name = a_name
+        self.__genre = a_genre
+        self.__platform = a_platform
 
     @property
     def name(self) -> str:
         return self.__name
+
+    @property
+    def genre(self) -> str:
+        return self.__genre
+
+    @property
+    def platform(self) -> str:
+        return self.__platform
 
 
 app = Flask(__name__)
@@ -15,9 +25,9 @@ app = Flask(__name__)
 
 @app.route("/start")
 def hello() -> str:
-    tetris = Game("Tetris")
-    skyrim = Game("Skyrim")
-    crash = Game("Crash Bandicoot")
+    tetris = Game("Tetris", "Puzzle", "Atari")
+    skyrim = Game("Skyrim", "RPG", "PS3")
+    crash = Game("Crash Bandicoot", "Platform", "PS1")
     games = [tetris, skyrim, crash]
     return render_template("list.html", a_title="Games", table_data=games)
 
--- tests/test_game_library.py
+++ tests/test_game_library.py
@@ -14,5 +14,11 @@ class TestGameLibrary(unittest.TestCase):
     def test_table_data(self):
         response = self.app.get("/start")
         assert b"<td>Tetris</td>" in response.data
+        assert b"<td>Puzzle</td>" in response.data
+        assert b"<td>Atari</td>" in response.data
         assert b"<td>Skyrim</td>" in response.data
+        assert b"<td>RPG</td>" in response.data
+        assert b"<td>PS3</td>" in response.data
         assert b"<td>Crash Bandicoot</td>" in response.data
+        assert b"<td>Platform</td>" in response.data
+        assert b"<td>PS1</td>" in response.data
