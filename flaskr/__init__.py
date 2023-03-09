4828bd86a66bff46ca64542364c7bb2062f41dea
alvesgf16 <alvesgabriel1994@gmail.com>
2023-03-09 15:40:01 -0300

    fix: remove broken top-level environment run
    Instead of running this app with python3, users should use flask cli.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -7,8 +7,3 @@ def create_app() -> Flask:
     app = Flask(__name__)
     app.register_blueprint(game_library.bp)
     return app
-
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
