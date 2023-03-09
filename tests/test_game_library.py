17b6408c19838f7edb4155ea344dd3f57b7453ea
alvesgf16 <alvesgabriel1994@gmail.com>
2023-03-09 20:59:43 -0300

    test: parameterize client get function

--- tests/test_game_library.py
+++ tests/test_game_library.py
@@ -9,19 +9,19 @@ class TestGameLibrary(unittest.TestCase):
         self.app = app.test_client()
 
     def test_page_header(self):
-        response = self.__when_the_test_client_calls_the_index_route()
+        response = self.__when_the_test_client_calls_a_route("/")
         self.__then_the_page_header_contains_the_correct_text(
             response, b"<h1>Games</h1>"
         )
 
     def test_table_headers(self):
-        response = self.__when_the_test_client_calls_the_index_route()
+        response = self.__when_the_test_client_calls_a_route("/")
         self.__then_the_cells_in_a_table_line_contain_the_correct_data(
             response, b"<th>Name</th>", b"<th>Genre</th>", b"<th>Platform</th>"
         )
 
     def test_table_data(self):
-        response = self.__when_the_test_client_calls_the_index_route()
+        response = self.__when_the_test_client_calls_a_route("/")
         self.__then_the_cells_in_a_table_line_contain_the_correct_data(
             response, b"<td>Tetris</td>", b"<td>Puzzle</td>", b"<td>Atari</td>"
         )
@@ -68,8 +68,8 @@ class TestGameLibrary(unittest.TestCase):
         )
         self.__then_the_correct_message_is_flashed(response, flashed_message)
 
-    def __when_the_test_client_calls_the_index_route(self):
-        return self.app.get("/")
+    def __when_the_test_client_calls_a_route(self, route):
+        return self.app.get(route)
 
     def __when_the_test_client_posts_on_a_route(self, route, data):
         return self.app.post(route, data=data, follow_redirects=True)
