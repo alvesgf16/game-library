395dc0664bb7c767c8469c93d1a0a5005ba5c933
alvesgf16 <alvesgabriel1994@gmail.com>
2023-03-10 12:42:02 -0300

    refactor: extract function redirect to origin

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -25,17 +25,21 @@ def login() -> Union[Response, str]:
                 return succesful_user_login(user.username)
         flash("User not logged in.")
         return redirect(url_for("auth.login"))
-    next_page = request.args.get("next_page") or ""
-    return render_template("login.html", a_title="Login", next_page=next_page)
+    origin = request.args.get("origin") or ""
+    return render_template("login.html", a_title="Login", origin=origin)
 
 
 def succesful_user_login(a_username: str) -> Response:
     set_session_user(a_username)
     flash(f"{a_username} logged in succesfully!")
+    return redirect_to_origin_of_request()
 
 
 def set_session_user(a_username: str) -> None:
     session["logged_in_user"] = a_username
+
+
+def redirect_to_origin_of_request() -> Response:
     origin = request.form["origin"]
     return redirect(origin)
 
