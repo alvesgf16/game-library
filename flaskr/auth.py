b0cc45bf2a8d1fe20460e5ba3b00edb72b4476d8
alvesgf16 <alvesgabriel1994@gmail.com>
2023-03-10 11:52:56 -0300

    refactor: extract function succesful user login

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -22,10 +22,7 @@ def login() -> Union[Response, str]:
         if request.form["username"] in [user.username for user in users]:
             user = users.get_by_username(request.form["username"])
             if request.form["password"] == user.password:
-                session["logged_in_user"] = user.username
-                flash(f"{user.username} logged in succesfully!")
-                next_page = request.form["next_page"]
-                return redirect(next_page)
+                return succesful_user_login(user.username)
             else:
                 flash("User not logged in.")
                 return redirect(url_for("auth.login"))
@@ -34,6 +31,13 @@ def login() -> Union[Response, str]:
             return redirect(url_for("auth.login"))
     next_page = request.args.get("next_page") or ""
     return render_template("login.html", a_title="Login", next_page=next_page)
+
+
+def succesful_user_login(a_username: str) -> Response:
+    session["logged_in_user"] = a_username
+    flash(f"{a_username} logged in succesfully!")
+    next_page = request.form["next_page"]
+    return redirect(next_page)
 
 
 @bp.route("/logout")
