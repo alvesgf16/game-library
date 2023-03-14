8cca00ef8f4d3ce20c5b6ad534dcbf1f599fe776
alvesgf16 <alvesgabriel1994@gmail.com>
2023-03-14 20:40:46 -0300

    refactor(db): extract creation functions

--- flaskr/db_new.py
+++ flaskr/db_new.py
@@ -3,6 +3,7 @@ from os import environ as env
 import click
 import mysql.connector
 from mysql.connector import errorcode
+from mysql.connector.abstracts import MySQLCursorAbstract
 
 GAMES = "games"
 GAMES_TABLE_CREATION_QUERY = """
@@ -46,13 +47,8 @@ def db_create() -> None:
             password=env.get("MYSQL_PASSWORD"),
         ) as connection:
             with connection.cursor() as cursor:
-                cursor.execute("DROP DATABASE IF EXISTS `game_library`;")
-                cursor.execute("CREATE DATABASE `game_library`;")
-                cursor.execute("USE `game_library`;")
-                for table in tables:
-                    print(f"Creating table {table.name}:", end=" ")
-                    cursor.execute(table.creation_query)
-                    print("OK")
+                create_database(cursor)
+                create_tables(cursor)
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print("There is something wrong with the username or password")
@@ -60,6 +56,23 @@ def db_create() -> None:
             print("Table already exists")
         else:
             print(err)
+
+
+def create_database(a_cursor: MySQLCursorAbstract) -> None:
+    a_cursor.execute("DROP DATABASE IF EXISTS `game_library`;")
+    a_cursor.execute("CREATE DATABASE `game_library`;")
+    a_cursor.execute("USE `game_library`;")
+
+
+def create_tables(a_cursor: MySQLCursorAbstract) -> None:
+    for table in tables:
+        create_table(table, a_cursor)
+
+
+def create_table(a_table: Table, a_cursor: MySQLCursorAbstract) -> None:
+    print(f"Creating table {a_table.name}:", end=" ")
+    a_cursor.execute(a_table.creation_query)
+    print("OK")
 
 
 @click.command("db-create")
