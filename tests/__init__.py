7786127dca1c38fa89f3c4bafe1eb6166b772368
alvesgf16 <alvesgabriel1994@gmail.com>
2023-03-14 18:11:46 -0300

    test: rename test client attribute

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,13 +6,13 @@ from flaskr import create_app
 class TestBase(unittest.TestCase):
     def setUp(self):
         app = create_app()
-        self.app = app.test_client()
+        self.client = app.test_client()
 
     def _when_the_test_client_calls_a_route(self, route):
-        return self.app.get(route, follow_redirects=True)
+        return self.client.get(route, follow_redirects=True)
 
     def _when_the_test_client_posts_on_a_route(self, route, data):
-        return self.app.post(route, data=data, follow_redirects=True)
+        return self.client.post(route, data=data, follow_redirects=True)
 
     def _then_the_page_header_contains_the_correct_text(
         self, response, header_text
