c114745faf2bb4e22212af15a8800e72bff93daa
alvesgf16 <alvesgabriel1994@gmail.com>
2023-03-15 18:41:03 -0300

    test: restructure db tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -7,42 +7,56 @@ from . import TestBase
 
 
 class TestDb(TestBase):
+    def setUp(self):
+        super().setUp()
+        connect_patcher = patch("mysql.connector.connect")
+        self.mock_connect = connect_patcher.start()
+        self.addCleanup(connect_patcher.stop)
+
     def test_db_create_succesful(self):
-        with patch("mysql.connector.connect") as mock_connect:
-            mock_connection = MagicMock()
-            mock_connect.return_value = mock_connection
-            result = self.runner.invoke(args=["db-create"])
-            assert "created" in result.output
-            assert mock_connect.called
+        self.base_success_case_test("db-create", "created")
 
     def test_db_create_access_denied(self):
-        with patch("mysql.connector.connect") as mock_connect:
-            mock_connect.side_effect = mysql.connector.Error(
-                errno=errorcode.ER_ACCESS_DENIED_ERROR
-            )
-            result = self.runner.invoke(args=["db-create"])
-            assert "something wrong" in result.output
+        self.__given_a_mocked_connection_error(
+            errorcode.ER_ACCESS_DENIED_ERROR
+        )
+        result = self.__when_a_command_is_invoked("db-create")
+        self.__then_the_correct_message_is_displayed(result, "something wrong")
 
     def test_db_create_table_already_exists(self):
-        with patch("mysql.connector.connect") as mock_connect:
-            mock_connect.side_effect = mysql.connector.Error(
-                errno=errorcode.ER_TABLE_EXISTS_ERROR
-            )
-            result = self.runner.invoke(args=["db-create"])
-            assert "Table already exists" in result.output
+        self.__given_a_mocked_connection_error(errorcode.ER_TABLE_EXISTS_ERROR)
+        result = self.__when_a_command_is_invoked("db-create")
+        self.__then_the_correct_message_is_displayed(
+            result, "Table already exists"
+        )
 
     def test_db_seed_successful(self):
-        with patch("mysql.connector.connect") as mock_connect:
-            mock_connection = MagicMock()
-            mock_connect.return_value = mock_connection
-            result = self.runner.invoke(args=["db-seed"])
-            assert "seeding complete" in result.output
-            assert mock_connect.called
+        self.base_success_case_test("db-seed", "seeding complete")
 
     def test_db_seed_access_denied(self):
-        with patch("mysql.connector.connect") as mock_connect:
-            mock_connect.side_effect = mysql.connector.Error(
-                errno=errorcode.ER_ACCESS_DENIED_ERROR
-            )
-            result = self.runner.invoke(args=["db-seed"])
-            assert "something wrong" in result.output
+        self.__given_a_mocked_connection_error(
+            errorcode.ER_ACCESS_DENIED_ERROR
+        )
+        result = self.__when_a_command_is_invoked("db-seed")
+        self.__then_the_correct_message_is_displayed(result, "something wrong")
+
+    def base_success_case_test(self, a_command, expected_text):
+        self.__given_a_mocked_database()
+        result = self.__when_a_command_is_invoked(a_command)
+        self.__then_the_correct_message_is_displayed(result, expected_text)
+        assert self.mock_connect.called
+
+    def __given_a_mocked_database(self):
+        mock_connection = MagicMock()
+        self.mock_connect.return_value = mock_connection
+
+    def __given_a_mocked_connection_error(self, an_error_code):
+        self.mock_connect.side_effect = mysql.connector.Error(
+            errno=an_error_code
+        )
+
+    def __when_a_command_is_invoked(self, a_command):
+        return self.runner.invoke(args=[a_command])
+
+    def __then_the_correct_message_is_displayed(self, actual, expected_text):
+        assert expected_text in actual.output
