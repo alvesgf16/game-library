29513fea7016288861dcb5631aaa97832cabd0f1
alvesgf16 <alvesgabriel1994@gmail.com>
2023-03-16 18:04:37 -0300

    fix: increase username character limit

--- flaskr/auth/models.py
+++ flaskr/auth/models.py
@@ -4,7 +4,7 @@ from flaskr import db
 class User(db.Model):
     __tablename__ = "users"
 
-    username = db.Column(db.String(8), primary_key=True)
+    username = db.Column(db.String(10), primary_key=True)
     name = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
