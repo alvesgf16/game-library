faa57510bf5b96c827934247feaecc593c14b836
alvesgf16 <alvesgabriel1994@gmail.com>
2023-03-27 17:15:30 -0300

    feat(uploader): add timestamp to created file name

--- flaskr/game_library/helpers.py
+++ flaskr/game_library/helpers.py
@@ -1,3 +1,5 @@
+import time
+
 from flask import current_app
 from werkzeug.datastructures import FileStorage
 
@@ -8,6 +10,7 @@ class GameCoverUploader:
         self.upload_path = current_app.config["UPLOAD_PATH"]
 
     def upload_cover_file(self, a_cover_art: FileStorage) -> None:
+        timestamp = time.time()
         a_cover_art.save(
-            f"{self.upload_path}/cover{self.game_id}.jpg"
+            f"{self.upload_path}/cover{self.game_id}-{timestamp}.jpg"
         )
