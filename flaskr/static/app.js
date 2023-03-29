8de3a1e2f5f8cb14b51472e3ce031ac54fdb3d47
alvesgf16 <alvesgabriel1994@gmail.com>
2023-03-28 22:40:06 -0300

    refactor: decompose js code to display cover

--- flaskr/static/app.js
+++ flaskr/static/app.js
@@ -1,15 +1,37 @@
-$('form input[type="file"]').on("change", ({ target }) => {
-  let { files } = target;
-  if (files.length === 0) {
+const createImageElement = (an_image_file) => {
+  const result = $('<img class="img-responsive">');
+  result.attr("src", window.URL.createObjectURL(an_image_file));
+  return result
+}
+
+const updateImage = (an_image_file) => {
+  $("img").remove();
+  const image = createImageElement(an_image_file);
+  $("figure").prepend(image);
+}
+
+const isJpegImage = (a_file) => a_file.type == "image/jpeg"
+
+const uploadImage = (a_file) => {
+  if (isJpegImage(a_file)) {
+    updateImage(a_file)
+  } else {
+    alert("Unsupported format");
+  }
+}
+
+const isEmpty = (an_array) => an_array.length === 0
+
+const handleImageUpload = (event) => {
+  const { files } = event.target;
+  
+  if (isEmpty(files)) {
     console.log("no image to display");
   } else {
-    if (files[0].type == "image/jpeg") {
-      $("img").remove();
-      let image = $('<img class="img-responsive">');
-      image.attr("src", window.URL.createObjectURL(files[0]));
-      $("figure").prepend(image);
-    } else {
-      alert("Unsupported format");
-    }
+    uploadImage(files[0])
   }
-});
+}
+
+$('form input[type="file"]').on("change", handleImageUpload);
+
+
