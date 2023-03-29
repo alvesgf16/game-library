a5afd9b320a7c0a50963f1301f0c7c7bcecc8358
alvesgf16 <alvesgabriel1994@gmail.com>
2023-03-29 18:20:16 -0300

    refactor: extract function to populate update form

--- flaskr/views/game_library.py
+++ flaskr/views/game_library.py
@@ -90,10 +90,7 @@ def add_game_to_database(a_game: Game) -> None:
 def update(id: int) -> Renderable:
     game = get_game_by_id(id)
     assert isinstance(game, Game)
-    form = GameForm()
-    form.name.data = game.name
-    form.genre.data = game.genre
-    form.platform.data = game.platform
+    form = create_game_form_from_game(game)
     game_cover = GameCoverUploader(id).retrieve_uploaded_cover_filename()
     if request.method == "POST":
         return update_game(game)
@@ -109,6 +106,14 @@ def update(id: int) -> Renderable:
 def get_game_by_id(an_id: int) -> Optional[Game]:
     select_game_by_id = db.select(Game).filter_by(id=an_id)
     return db.session.execute(select_game_by_id).scalar()
+
+
+def create_game_form_from_game(a_game: Game) -> GameForm:
+    result = GameForm()
+    result.name.data = a_game.name
+    result.genre.data = a_game.genre
+    result.platform.data = a_game.platform
+    return result
 
 
 def update_game(a_game: Game) -> Response:
